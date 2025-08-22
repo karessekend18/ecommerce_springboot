95dbf7f4f85eee60585039b2e0f24d7f6d39217f
Karesse DSouza <kadsouza+cisco@cisco.com>
2025-08-22 12:01:11 +0530

    Default values added for pageNumber & pageSize

--- src/main/java/com/ecommerce/sb_ecomm/controller/CategoryController.java
+++ src/main/java/com/ecommerce/sb_ecomm/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package com.ecommerce.sb_ecomm.controller;
 
+import com.ecommerce.sb_ecomm.config.AppConstants;
 import com.ecommerce.sb_ecomm.dto.CategoryRequest;
 import com.ecommerce.sb_ecomm.dto.CategoryResponse;
 import com.ecommerce.sb_ecomm.service.CategoryService;
@@ -18,8 +19,8 @@ public class CategoryController {
     private CategoryService categoryService;
 
     @GetMapping("/public/categories")
-    public ResponseEntity<CategoryResponse> getAllCategories(@RequestParam(name = "pageNumber") Integer pageNumber,
-                                                             @RequestParam(name = "pageSize") Integer pageSize) {
+    public ResponseEntity<CategoryResponse> getAllCategories(@RequestParam(name = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+                                                             @RequestParam(name = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize) {
         CategoryResponse categoryResponse = categoryService.getAllCategories(pageNumber, pageSize);
         return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
     }
